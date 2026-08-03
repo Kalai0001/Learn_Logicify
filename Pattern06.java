4986e3fff5f0fa31b08c3a46c0533e96cdb4dcec
Kalai0001 <kalai1172000@gmail.com>
2026-08-03 21:22:30 +0530

    Add files via upload

---
+++ Pattern06.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Pattern06{
+    void main(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        Pattern06 obj=new Pattern06();
+        obj.main(n);
+        sc.close();
+    }
+}
