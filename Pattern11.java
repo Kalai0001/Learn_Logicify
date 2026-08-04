a4a6a63d50b1de63fd6555c8935190b5daee3463
Kalai0001 <kalai1172000@gmail.com>
2026-08-04 21:47:01 +0530

    Add files via upload

---
+++ Pattern11.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Pattern11{
+    void main(int n){
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        Pattern11 obj=new Pattern11();
+        obj.main(n);
+        sc.close();
+    }
+}
