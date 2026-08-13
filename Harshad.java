3a801a0ae5fd58782909a42a462811151632c33e
Kalai0001 <kalai1172000@gmail.com>
2026-08-13 20:44:15 +0530

    Reformat Harshad.java with consistent spacing

--- Harshad.java
+++ Harshad.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class Harshad{
+    
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         long n=sc.nextLong();
@@ -7,6 +8,7 @@ public class Harshad{
         obj.harshad(n);
         sc.close();
     }
+    
     void harshad(long n){
         long temp=n;
         long res=harshadno(n);
@@ -17,6 +19,7 @@ public class Harshad{
             System.out.printf("No");
         }
     }
+    
     public static long harshadno(long n){
         long sum=0;
         while(n!=0){
