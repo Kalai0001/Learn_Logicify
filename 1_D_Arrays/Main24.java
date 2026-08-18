52d548933cba640b7ac959edd5e5f213129da8dc
Kalai0001 <kalai1172000@gmail.com>
2026-08-18 16:46:39 +0530

    Add files via upload

---
+++ 1_D_Arrays/Main24.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main24{
+    static void main(int n,long arr[]){
+       
+        long sumarr[]=new long[n];
+        for(int i=0;i<n;i++){
+            long sum=0;
+            for(int j=0;j<i;j++){
+                if(i!=j){
+                    sum=sum+arr[j];
+                }
+            }
+            sumarr[i]=sum;
+        }
+        
+        for(int i=0;i<n;i++){
+            System.out.print(sumarr[i]+" ");
+        }
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        long arr[]=new long[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextLong();
+        }
+        main(n,arr);
+        sc.close();
+    }
+}
