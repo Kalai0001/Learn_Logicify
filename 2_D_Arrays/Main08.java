db0f4ef1de86111b2bae7cc9c85153c9ee6f7c23
Kalai <kalai1172000@gmail.com>
2026-08-24 12:19:39 +0530

    Add files via upload

---
+++ 2_D_Arrays/Main08.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main08{
+    static void main(int r,int c,long arr[][]){
+        int k = 0;
+        for(int i = 0; i < r; i++){
+            for(int j = k; j < c; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            k++;
+            System.out.println();
+        }
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        long arr[][] = new long[r][c];
+        
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                arr[i][j] = sc.nextLong();
+            }
+        }
+        main(r,c,arr);
+        sc.close();
+    }
+}
