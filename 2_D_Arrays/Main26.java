c88cf7e083d9776385fd7223694da257eaf9aaac
Kalai <kalai1172000@gmail.com>
2026-08-26 19:55:07 +0530

    Add files via upload

---
+++ 2_D_Arrays/Main26.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Main26{
+    static void main(int r,int c,long arr[][]){
+        
+        for(int i = 0; i < c; i++){
+            for(int j = 0; j < r; j++){
+                for(int k = 0; k < j; k++){
+                    if(arr[j][i] < arr[k][i]){
+                        long tmp = arr[j][i];
+                        arr[j][i] = arr[k][i];
+                        arr[k][i] = tmp;
+                    }
+                }
+            }
+        }
+        
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        long arr[][] = new long[r][c];
+        for(int i = 0; i< r; i++){
+            for(int j = 0; j < c; j++){
+                arr[i][j] = sc.nextLong();
+            }
+        }
+        main(r,c,arr);
+        sc.close();
+    }
+}
