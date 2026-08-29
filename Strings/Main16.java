5a7b51568e0e63e41d320a011c7a304c6f18eb6b
Kalai <kalai1172000@gmail.com>
2026-08-29 21:19:03 +0530

    Add files via upload

---
+++ Strings/Main16.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class Main16{
+    public static int stringlength(String s){
+        int count = 0;
+        char arr[] = s.toCharArray();
+        for(int i = 0; i < arr.length; i++){
+            char ch = arr[i];
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int ans = stringlength(s);
+        System.out.print(ans);
+        sc.close();
+    }
+}
